f0431dd0c9941f32d00b9514f9383ad2c7759fee
Arshdeep Singh <arshdeepjhagra@gmail.com>
2025-09-05 03:53:00 +0400

    courses on footer

--- resources/views/theme/xacademia/layouts/footer.blade.php
+++ resources/views/theme/xacademia/layouts/footer.blade.php
@@ -3,7 +3,7 @@
 <!--Section-->
 <div class="position-relative">
     <div class="shape overflow-hidden bottom-footer-shape">
-        <svg viewBox="0 0 2880 48" fill="none" xmsns="http://www.w3.org/2000/svg">
+        <svg viewBox="0 0 2880 48" fill="none" xmlns="http://www.w3.org/2000/svg">
             <path d="M0 48H1437.5H2880V0H2160C1442.5 52 720 0 720 0H0V48Z" fill="currentColor"></path>
         </svg>
     </div>
@@ -15,178 +15,93 @@
     <div class="footer-main footer-main1">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
-                    <div class="row">
-                        <div class="col-lg-7 col-md-12">
-                            <img src="{{ asset('theme/xacademia/assets/images/brand/logo-white.png') }}"
-                                class="w-auto border-0" alt="img">
-                            <ul class="list-unstyled mb-0 mt-4">
-                                <li>
-                                    <a href="javascript:void(0)">{{ setting('address') }}</a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0)">{{ setting('contact_email') }} </a>
-                                </li>
-                                <li>
-                                    <a href="javascript:void(0)">{{ setting('contact_phone') }}</a>
-                                </li>
-                            </ul>
-                            <ul class="list-unstyled list-inline mt-5 mb-lg-0 mb-5">
-                                @if (!empty(setting('facebook')))
-                                    <li class="list-inline-item">
-                                        <a href="{{ setting('facebook') }}"
-                                            class="social-icons btn-sm rgba-white-slight waves-effect waves-light">
-                                            <i class="fa fa-facebook"></i>
-                                        </a>
-                                    </li>
-                                @endif
-                                @if (!empty(setting('instagram')))
-                                    <li class="list-inline-item">
-                                        <a href="{{ setting('instagram') }}"
-                                            class="social-icons btn-sm rgba-white-slight waves-effect waves-light">
-                                            <i class="fa fa-instagram"></i>
-                                        </a>
-                                    </li>
-                                @endif
-                                @if (!empty(setting('linkedin')))
-                                    <li class="list-inline-item">
-                                        <a href="{{ setting('linkedin') }}"
-                                            class="social-icons btn-sm rgba-white-slight waves-effect waves-light">
-                                            <i class="fa fa-linkedin"></i>
-                                        </a>
-                                    </li>
-                                @endif
-                                @if (!empty(setting('twitter')))
-                                    <li class="list-inline-item">
-                                        <a href="{{ setting('twitter') }}"
-                                            class="social-icons btn-sm rgba-white-slight waves-effect waves-light">
-                                            <i class="fa fa-twitter"></i>
-                                        </a>
-                                    </li>
-                                @endif
-                            </ul>
-                        </div>
-                        <div class="col-lg-5 col-md-12">
-                            <h6 class="font-weight-normal text-white">Categories</h6>
-                            <ul class="list-unstyled mb-5 mb-lg-0">
-                                @foreach (menu_cataloge() as $category)
-                                    <li>
-                                        <a href="{{ route('categories.show', ['category' => $category->slug]) }}">
-                                            <i class="fe fe-chevron-right"></i>
-                                            {{ $category->translation->name }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    </div>
+                <!-- Logo + Contact -->
+                <div class="col-lg-2 col-md-12 mb-5 mb-lg-0">
+                    <img src="{{ asset('theme/xacademia/assets/images/brand/logo-white.png') }}" class="w-auto border-0" alt="logo">
+                    <ul class="list-unstyled mb-0 mt-4 text-white">
+                        <li><i class="fe fe-map-pin"></i> {{ setting('address') }}</li>
+                        <li><i class="fe fe-mail"></i> {{ setting('contact_email') }}</li>
+                        <li><i class="fe fe-phone"></i> {{ setting('contact_phone') }}</li>
+                    </ul>
+                    <ul class="list-inline mt-4">
+                        @if (setting('facebook'))
+                            <li class="list-inline-item"><a href="{{ setting('facebook') }}" class="social-icons"><i class="fa fa-facebook"></i></a></li>
+                        @endif
+                        @if (setting('instagram'))
+                            <li class="list-inline-item"><a href="{{ setting('instagram') }}" class="social-icons"><i class="fa fa-instagram"></i></a></li>
+                        @endif
+                        @if (setting('linkedin'))
+                            <li class="list-inline-item"><a href="{{ setting('linkedin') }}" class="social-icons"><i class="fa fa-linkedin"></i></a></li>
+                        @endif
+                        @if (setting('twitter'))
+                            <li class="list-inline-item"><a href="{{ setting('twitter') }}" class="social-icons"><i class="fa fa-twitter"></i></a></li>
+                        @endif
+                    </ul>
                 </div>
-                <div class="col-lg-6">
-                    <div class="row">
-                        <div class="col-lg-8 col-md-12">
-                            <h6 class="font-weight-normal text-white">Courses</h6>
-                            <ul class="list-unstyled mb-5 mb-lg-0">
-                                @foreach (menu_cataloge() as $category)
-                                    <li>
-                                        <a href="{{ route('categories.show', ['category' => $category->slug]) }}">
-                                            <i class="fe fe-chevron-right"></i>
-                                            {{ $category->translation->name }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        <div class="col-lg-4 col-md-12">
-                            <h6 class="font-weight-normal text-white">Our Company</h6>
-                            <ul class="list-unstyled mb-lg-0 mb-5">
-                                <li><a href="javascript:;"><i class="fe fe-chevron-right"></i> Our Team</a></li>
-                                <li><a href="javascript:;"><i class="fe fe-chevron-right"></i> Contact US</a></li>
-                                <li><a href="javascript:;"><i class="fe fe-chevron-right"></i> About Us</a></li>
-                                <li><a href="javascript:;"><i class="fe fe-chevron-right"></i> Services</a></li>
-                                <li><a href="javascript:;"><i class="fe fe-chevron-right"></i> Blog News</a></li>
-                                <li><a href="javascript:;"><i class="fe fe-chevron-right"></i> Online Classes</a></li>
-                            </ul>
-                        </div>
-                    </div>
+
+                <!-- Categories -->
+                <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+                    <h6 class="font-weight-normal text-white mb-3">Categories</h6>
+                    <ul class="list-unstyled">
+                        @foreach (menu_cataloge() as $category)
+                            <li>
+                                <a href="{{ route('categories.show', ['category' => $category->slug]) }}">
+                                    <i class="fe fe-chevron-right"></i>
+                                    {{ $category->translation->name }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+
+                <!-- Featured Courses -->
+                <div class="col-lg-4 col-md-6 mb-5 mb-lg-0">
+                    <h6 class="font-weight-normal text-white mb-3">Featured Courses</h6>
+                    <ul class="list-unstyled">
+                        @foreach (courses()->where('is_featured', true)->take(5) as $course)
+                            <li>
+                                <a href="{{ route('courses.show', $course->slug) }}">
+                                    <i class="fe fe-star text-warning"></i>
+                                    {{ $course->name }}
+                                </a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </div>
+
+                <!-- Our Company -->
+                <div class="col-lg-2 col-md-6">
+                    <h6 class="font-weight-normal text-white mb-3">Our Company</h6>
+                    <ul class="list-unstyled">
+                        <li><a href="{{ route('about') }}"><i class="fe fe-chevron-right"></i> About Us</a></li>
+                        <li><a href="{{ route('contact') }}"><i class="fe fe-chevron-right"></i> Contact Us</a></li>
+                        <li><a href="#"><i class="fe fe-chevron-right"></i> Our Team</a></li>
+                        <li><a href="#"><i class="fe fe-chevron-right"></i> Services</a></li>
+                        <li><a href="#"><i class="fe fe-chevron-right"></i> Blog News</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
     </div>
 
+    <!-- Bottom -->
     <div class="py-4 footer-main border-bottom bg-dark p-0 border-top-0 border-white-1">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg">
-                    <ul class="payments mb-0 mt-2">
-                        <li>
-                            <a href="javascript:void(0)" class="payments-icon text-white-50"><i class="fa fa-cc-amex"
-                                    aria-hidden="true"></i></a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)" class="payments-icon text-white-50"><i class="fa fa-cc-visa"
-                                    aria-hidden="true"></i></a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)" class="payments-icon text-white-50"><i
-                                    class="fa fa-credit-card-alt" aria-hidden="true"></i></a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)" class="payments-icon text-white-50"><i
-                                    class="fa fa-cc-mastercard" aria-hidden="true"></i></a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)" class="payments-icon text-white-50"><i class="fa fa-cc-paypal"
-                                    aria-hidden="true"></i></a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)" class="payments-icon text-white-50"><i
-                                    class="fa fa-cc-discover" aria-hidden="true"></i></a>
-                        </li>
-                        <li>
-                            <a href="javascript:void(0)" class="payments-icon text-white-50"><i
-                                    class="fa fa-google-wallet" aria-hidden="true"></i></a>
-                        </li>
-                    </ul>
-                </div>
-                <div class="col-lg col-lg-auto">
-                    <div class="form-group d-flex mb-0">
-                        {{-- <div class="me-4 w-200">
-								<select class="form-control select2" data-placeholder="Select Country">
-									<option value="en">English</option>
-									<option value="en">Arabic</option>
-									<option value="en">German</option>
-									<option value="en">Greek</option>
-								</select>
-							</div>
-							<div class="me-4 w-200">
-								<select class="form-control select2" data-placeholder="Select Country">
-									<option value="en">USD</option>
-									<option value="en">EUR</option>
-									<option value="en">INR</option>
-									<option value="en">GBP</option>
-								</select>
-							</div> --}}
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="py-3 bg-dark">
-        <div class="container">
-            <div class="row d-flex">
-                <div class="col-lg-12 col-sm-12  mt-2 mb-2 text-center text-white">
-                    @if (!empty(setting('copyright')))
-                        {!! setting('copyright') !!}
-                    @else
-                        Copyright © {{ now()->year }} <a href="javascript:void(0)"
-                            class="fs-14 text-primary">{{ env('APP_NAME') }}</a>. All rights reserved.
-                    @endif
-                </div>
+        <div class="container d-flex justify-content-between align-items-center">
+            <ul class="payments mb-0">
+                <li><i class="fa fa-cc-visa text-white-50"></i></li>
+                <li><i class="fa fa-cc-mastercard text-white-50"></i></li>
+                <li><i class="fa fa-cc-paypal text-white-50"></i></li>
+            </ul>
+            <div class="text-white">
+                @if (!empty(setting('copyright')))
+                    {!! setting('copyright') !!}
+                @else
+                    © {{ now()->year }} <a href="javascript:void(0)" class="text-primary">{{ env('APP_NAME') }}</a>. All rights reserved.
+                @endif
             </div>
         </div>
     </div>
 </footer>
-<!--Footer Section-->
 
 <!-- Back to top -->
 <a href="#top" id="back-to-top"><i class="fa fa-long-arrow-up"></i></a>
